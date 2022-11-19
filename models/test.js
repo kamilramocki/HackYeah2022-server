caa9ea2ed091896bcb6fd911b02ebb63e5279735
Kamil Ramocki <ramockikamil@gmail.com>
2022-11-19 20:53:45 +0100

    Fixed variable names

--- models/test.js
+++ models/test.js
@@ -3,8 +3,8 @@ const { Schema } = require('mongoose');
 const Test = mongoose.model('Test', {
     title: String,
     questions: [{ type: Schema.Types.ObjectId, ref: 'Question' }],
-    join_code: String,
-    generation_date: Date,
+    joinCode: String,
+    generationDate: Date,
     difficulty: String,
 });
 
