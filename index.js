d025d2e8f0894fb3cdac6072bf47b977679bdf7e
Kamil Ramocki <ramockikamil@gmail.com>
2022-11-20 11:03:34 +0100

    Updated bodyParser to json

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.use(categoriesRouter);
